c219a4ee52cb98d1d6519b6717a0b181a64e5e5b
PeterASteele <peter707@vt.edu>
2016-08-17 20:50:43 -0400

    Added accuracy stat

--- CodeforcesAPItest.py
+++ CodeforcesAPItest.py
@@ -20,12 +20,18 @@ if __name__ == '__main__':
         response_json = response.read();
         response_json = str(response_json.decode("utf-8"))
         parsed_response = json.loads(response_json)
+        ok = 0;
+        bad = 0;
         if parsed_response['result'] != []:
             for i in parsed_response['result']:
                 if i['author']['participantType'] == "CONTESTANT":
-                    # only if it is a legit submit
-                    print(str(i))              
-            
+                    if i['testset'] == "TESTS":
+                        if i['verdict'] == 'OK':
+                            ok = ok + 1
+                        else:
+                            bad = bad + 1
+#                         print(str(i))              
+        print("AC: " + str(ok) + " BAD: " + str(bad) + " TOTAL: " + str(ok + bad) + " Correct: " + str(100*ok/(ok+bad)) + "%")
     except:
         e = sys.exc_info()[0]
         print(str(e))
